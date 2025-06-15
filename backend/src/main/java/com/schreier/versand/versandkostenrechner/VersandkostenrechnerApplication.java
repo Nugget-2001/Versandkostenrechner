e26112ef995a7f5f37e2ea3cd01bc2d565892b61
Nico Schreier <nico.schreier@stud.fh-campuswien.ac.at>
2025-06-15 11:09:16 +0200

    Initial Commit

---
+++ backend/src/main/java/com/schreier/versand/versandkostenrechner/VersandkostenrechnerApplication.java
@@ -0,0 +1,13 @@
+package com.schreier.versand.versandkostenrechner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersandkostenrechnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersandkostenrechnerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/schreier/versand/versandkostenrechner/VersandkostenrechnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schreier.versand.versandkostenrechner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersandkostenrechnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
